c2932afdd471dbc30fbae18ca213c52b335b118b
Sowjanya <mrcraosowji@gmail.com>
2023-06-19 10:08:57 +0530

    first message

---
+++ src/test/java/com/JwtAuth/JwtYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JwtAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtYoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
